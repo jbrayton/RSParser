305ef9e85fac6d3e247298976a5f054a0184bdb3
John Brayton <jbrayton@goldenhillsoftware.com>
2025-08-08 19:53:34 -0400

    take 3

---
+++ RSParser-Mac/RSParser_Mac.swift
@@ -0,0 +1,10 @@
+//
+//  RSParser_Mac.swift
+//  RSParser-Mac
+//
+//  Created by John Brayton on 8/8/25.
+//  Copyright © 2025 Ranchero Software, LLC. All rights reserved.
+//
+
+import Foundation
+
